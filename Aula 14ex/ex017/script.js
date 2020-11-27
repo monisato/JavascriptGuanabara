6a9428682be8678881b017ebb7beb99e0a25272b
unknown <monicasakane@gmail.com>
2020-11-27 12:49:30 -0300

    aula14 ex2

--- Aula 14ex/ex017/script.js
+++ Aula 14ex/ex017/script.js
@@ -1,1 +1,34 @@
-window.alert('Olá!')
+let num = document.getElementById('num')
+let res = document.getElementById('res')
+
+// minha solução
+function gerar() {
+    if (num.value.length == 0) {
+        window.alert('Digite um número!')
+    } else {
+        let n = Number(num.value)
+        res.innerHTML = ''        
+        for (let i = 0; i <= 10; i++) {
+            res.innerHTML += `<option value="${i}">${i} x ${n} = ${i*n}</option>`
+        }
+    }
+}
+
+/* solução do guanabara
+function gerar() {
+    if (num.value.length == 0) {
+        window.alert('Digite um número!')
+    } else { 
+        let n = Number(num.value)
+        let c = 0
+        res.innerHTML = ''
+        while (c <= 10) {
+            let opt = document.createElement('option')
+            opt.setAttribute('value', `${c}`)
+            opt.text = `${c} x ${n} = ${c*n}`
+            res.appendChild(opt)
+            c++
+        }
+    }
+}
+*/
